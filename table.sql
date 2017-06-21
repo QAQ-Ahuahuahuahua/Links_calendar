cb25c25bc109333a209b136f1ed1e72844cdcedc
阿华华华华 <382853730@qq.com>
2017-06-21 23:14:46 +0100

    update pages and sql

--- table.sql
+++ table.sql
@@ -6,10 +6,12 @@ CREATE TABLE users (
   uid serial NOT NULL,
   username varchar(32) NOT NULL,
   password varchar(32) NOT NULL DEFAULT '',
-  admin smallint NOT NULL DEFAULT '0',
+  admin boolean DEFAULT false,
   password_editable smallint NOT NULL DEFAULT '1',
   timezone varchar(255) DEFAULT NULL,
-  PRIMARY KEY (uid)
+  PRIMARY KEY (uid),
+  UNIQUE(username)
 );
 
-INSERT INTO users VALUES (1, 'zjh','123',1,1,null);
+INSERT INTO users VALUES (1, 'zjh','123',true,1,null);
+INSERT INTO users VALUES (2, 'doris','123',false,1,null);
