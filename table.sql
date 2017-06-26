a98529c45beb3258ad6d22d261720de10e388e2a
阿华华华华 <382853730@qq.com>
2017-06-26 20:21:41 +0100

    修复了login的问题

--- table.sql
+++ table.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
   uid serial NOT NULL,
   username varchar(32) NOT NULL,
   password varchar(32) NOT NULL DEFAULT '',
-  admin boolean DEFAULT false,
+  admin smallint DEFAULT '0',
   password_editable smallint NOT NULL DEFAULT '1',
   timezone varchar(255) DEFAULT NULL,
   PRIMARY KEY (uid),
@@ -33,7 +33,7 @@ CREATE TABLE user_group(
   uid int NOT NULL
 );
 
-INSERT INTO users VALUES (1, 'zjh','123',true,1,null);
-INSERT INTO users VALUES (2, 'doris','123',false,1,null);
+INSERT INTO users VALUES (1, 'zjh','123',1,1,null);
+INSERT INTO users VALUES (2, 'doris','123',0,1,null);
 
 INSERT INTO groups VALUES(1,'friends');
