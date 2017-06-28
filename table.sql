af5f5c4263e78554c17a80a662f926086b9ce410
阿华华华华 <382853730@qq.com>
2017-06-28 21:35:46 +0100

    修改group页面，并且添加部分permission

--- table.sql
+++ table.sql
@@ -20,20 +20,29 @@ DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
   gid serial NOT NULL,
   group_name varchar(32) NOT NULL,
+  creater smallint NOT NULL,
   PRIMARY KEY (gid),
   UNIQUE(group_name)
 );
 
 /*
-user_group
+user_group_permission
 */
 DROP TABLE IF EXISTS user_group;
 CREATE TABLE user_group(
   gid int NOT NULL,
-  uid int NOT NULL
+  uid int NOT NULL,
+  write smallint DEFAULT '0',
+  read smallint DEFAULT '1',
+  modify smallint DEFAULT '0',
+  admin smallint DEFAULT '0'
 );
+
 
 INSERT INTO users VALUES (1, 'zjh','123',1,1,null);
 INSERT INTO users VALUES (2, 'doris','123',0,1,null);
 
-INSERT INTO groups VALUES(1,'friends');
+INSERT INTO groups VALUES(1,'friends',1);
+
+INSERT INTO user_group VALUES (1,1,1,1,1,1);
+INSERT INTO user_group VALUES (1,2,1,1,1,1);
